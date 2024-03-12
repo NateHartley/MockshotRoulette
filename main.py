98bd448c198825b549210128578b21a78042dd2c
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2024-03-12 00:17:41 +0000

    Created player class to hold name and health data

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import shotgun
+import player
 import random
 import time
 
 def initialise():
-    p1name = input("\nEnter your name: ")
-    p2name = "The Dealer"
-    print("\n" + p1name , "will be playing against", p2name + ".\n")
     gun = shotgun.Shotgun(["L", "B", "B"], 3)
-    game(p1name, p2name, gun)
+    player_1 = player.Player()
+    player_2 = player.Player()
+    player_1.name = input("\nEnter your name: ")
+    player_2.name = "The Dealer"
+    print("\n" + player_1.name , "will be playing against", player_2.name + ".\n")
+
+    game(gun, player_1, player_2)
 
 
-def check_win(p1name, p2name, p1health, p2health):
-    if p1health == 0:
-        print("-----", p2name, "wins -----\n")
+def check_win(player_1: player.Player, player_2: player.Player):
+    if player_1.health == 0:
+        print("-----", player_1.name, "wins -----\n")
         exit
-    elif p2health == 0:
-        print("-----", p1name, "wins -----\n")
+    elif player_2.health == 0:
+        print("-----", player_2.name, "wins -----\n")
         exit
 
 
-def set_health(round):
-    if round == 1:
-        p1health = 2
-        p2health = 2
-        return p1health, p2health
-
-
-def player1Choice(p1name, p2name, p1health, p2health, gun):
+def player1Choice(player_1: player.Player, player_2: player.Player, gun: shotgun.Shotgun):
     choice = input()
     time.sleep(1.5)
     result = gun.shoot()
@@ -35,63 +32,63 @@ def player1Choice(p1name, p2name, p1health, p2health, gun):
     if choice == "1" and result == "L": # Shot self with live round
         print("You shot yourself with a live round...\n")
         time.sleep(1.5)
-        p1health -= 1
-        print("You have lost 1 health, remaining health =", p1health, "\n")
+        player_1.health -= 1
+        print("You have lost 1 health, remaining health =", player_1.health, "\n")
         time.sleep(1.5)
     elif choice == "1" and result == "B": # Shot self with blank round
         print("You shot yourself with a blank round...\n")
         time.sleep(1.5)
     elif choice == "2" and result == "L": # Shot player2 with live round
-        print("You shot", p2name, "with a live round...\n")
+        print("You shot", player_2.name, "with a live round...\n")
         time.sleep(1.5)
-        p2health -= 1
-        print(p2name, "has lost 1 health, remaining health =", p2health, "\n")
+        player_2.health -= 1
+        print(player_2.name, "has lost 1 health, remaining health =", player_2.health, "\n")
         time.sleep(1.5)
     elif choice == "2" and result == "B": # Shot player2 with blank round
-        print("You shot", p2name, "with a blank round...\n")
+        print("You shot", player_2.name, "with a blank round...\n")
         time.sleep(1.5)
     else:
         print("--Player1 error choice/result--")
 
-    return p1health, p2health
+    return player_1.health, player_2.health
 
 
-def player2Choice(p1name, p2name, p1health, p2health, gun):
+def player2Choice(player_1: player.Player, player_2: player.Player, gun: shotgun.Shotgun):
     choice = random.randint(1, 2)
     time.sleep(1.5)
     result = gun.shoot()
 
     if choice == 1:
-        print(p2name, "has chosen to shoot you.\n")
+        print(player_2.name, "has chosen to shoot you.\n")
         time.sleep(1.5)
     elif choice == 2:
-        print(p2name, "has chosen to shoot themself.\n")
+        print(player_2.name, "has chosen to shoot themself.\n")
         time.sleep(1.5)
     else:
         print("--Player2 error choice--")
 
     if choice == 1 and result == "L": # Player2 shot you with live round
-        print(p2name, "shot you with a live round...\n")
+        print(player_2.name, "shot you with a live round...\n")
         time.sleep(1.5)
-        p1health -= 1
-        print("You have lost 1 health, remaining health =", p1health, "\n")
+        player_1.health -= 1
+        print("You have lost 1 health, remaining health =", player_1.health, "\n")
         time.sleep(1.5)
     elif choice == 1 and result == "B": # Player2 shot you with blank round
-        print(p2name, "shot you with a blank round...\n")
+        print(player_2.name, "shot you with a blank round...\n")
         time.sleep(1.5)
     elif choice == 2 and result == "L": # Player2 shot self with live round
-        print(p2name, "shot themself with a live round...\n")
+        print(player_2.name, "shot themself with a live round...\n")
         time.sleep(1.5)
-        p2health -= 1
-        print(p2name, "has lost 1 health, remaining health =", p2health, "\n")
+        player_2.health -= 1
+        print(player_2.name, "has lost 1 health, remaining health =", player_2.health, "\n")
         time.sleep(1.5)
     elif choice == 2 and result == "B": # Player2 shot self with blank round
-        print(p2name, "shot themself with a blank round...\n")
+        print(player_2.name, "shot themself with a blank round...\n")
         time.sleep(1.5)
     else:
         print("--Player2 error choice/result--")
 
-    return p1health, p2health
+    return player_1.health, player_2.health
 
 
 def reload_gun(reload):
@@ -102,8 +99,10 @@ def reload_gun(reload):
     else:
         print("--Reload error--")
 
+    # put this in shotgun class as well
 
-def game(p1name, p2name, gun):
+
+def round_1():
     time.sleep(1.5)
     print("""
     +----------------+
@@ -114,13 +113,33 @@ def game(p1name, p2name, gun):
     print("Both player's health will start at 2.")
     print("3 shots are loaded into the shotgun; 1 live, 2 blank. \n")
     time.sleep(1.5)
-    turn = "p2" # set to p2 initially because it will get imediently flipped
-    reload = 0
 
     round = 1
-    p1health, p2health = set_health(round)
+    return round
 
-    while(p1health > 0 and p2health > 0):
+
+def round_2():
+    time.sleep(1.5)
+    print("""
+    +----------------+
+    | ROUND 2 BEGINS |
+    +----------------+
+    """)
+    time.sleep(1.5)
+    print("Both player's health will start at 4.\n")
+    print("The shotgun has been emptied...\n")
+    print("2 shots are loaded into the shotgun; 1 live, 1 blank. \n")
+    time.sleep(1.5)
+
+
+def game(gun: shotgun.Shotgun, player_1: player.Player, player_2: player.Player):
+    turn = "p2" # set to p2 initially because it will get imediently flipped
+    reload = 0
+    round = round_1()
+    player_1.set_health(round) # works! both set to 2 for round 1
+    player_2.set_health(round)
+
+    while(player_1.health > 0 and player_2.health > 0):
         turn = "p2" if turn == "p1" else "p1" # Flips turn
 
         if gun.total_rounds > 0:
@@ -128,14 +147,14 @@ def game(p1name, p2name, gun):
             if turn == "p1":
                 print("It is your turn.\n")
                 time.sleep(1.5)
-                print("Shoot yourself [1] or", p2name + "? [2] \n")
-                p1health, p2health = player1Choice(p1name, p2name, p1health, p2health, gun)
+                print("Shoot yourself [1] or", player_2.name + "? [2] \n")
+                player_1.health, player_2.health = player1Choice(player_1, player_2, gun)
 
             if turn == "p2":
-                print("It is", p2name + "'s turn.\n")
+                print("It is", player_2.name + "'s turn.\n")
                 time.sleep(1.5)
-                print(p2name, "is deciding...\n")
-                p1health, p2health = player2Choice(p1name, p2name, p1health, p2health, gun)
+                print(player_2.name, "is deciding...\n")
+                player_1.health, player_2.health = player2Choice(player_1, player_2, gun)
         else:
             print("The shotgun has been emptied...\n")
             reload += 1
@@ -145,7 +164,7 @@ def game(p1name, p2name, gun):
             time.sleep(1.5)
             exit
     else:
-        check_win(p1name, p2name, p1health, p2health)
+        check_win(player_1, player_2)
 
 
 def main():
---
+++ player.py
@@ -0,0 +1,21 @@
+class Player:
+    def __init__(self):
+        self.name = ""
+        self.health = None
+
+
+    def set_health(self, round):
+        if round == 1:
+            self.health = 2
+        elif round == 2:
+            self.health = 4
+        elif round == 3:
+            self.health = 6
+            # last two health cannot be regenerated though
+        else:
+            print("--set_health error--")
+
+
+    # loose_health()
+            
+    # gain_health()
