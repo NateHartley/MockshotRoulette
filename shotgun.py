13d8cee2533da5b6a17e69928364322502966335
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2024-03-13 21:39:05 +0000

    Comments

--- shotgun.py
+++ shotgun.py
@@ -7,12 +7,17 @@ class Shotgun:
 
     # Instance method
     def shoot(self):
+        # TODO: Occasionally will get an out of range error with rand, need to fix
         if len(self.rounds) > 0:
             rand = random.randint(0, self.total_rounds-1) # 1-3 is out of range, needs to be 0-2
+            print(">>> rand: ", rand)
+            print(">>> total rounds: ", self.total_rounds)
             chosen_round = self.rounds[rand]
+            print(">>> chosen round: ", chosen_round)
             self.total_rounds -= 1
             del self.rounds[rand]
             return chosen_round
         else:
             print("No more shots left")
             exit
+        # Get rid of if else once above todo is done
