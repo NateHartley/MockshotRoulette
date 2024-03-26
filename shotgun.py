1597bf76e44823dfe8b76bd8da8641aa8ce6db00
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2024-03-26 18:19:38 +0000

    More reloads

--- shotgun.py
+++ shotgun.py
@@ -14,44 +14,39 @@ class Shotgun:
             del self.rounds[rand]
             return chosen_round
         else:
-            print("No more shots left")
-            exit
+            print("--Shoot error--")
+            quit()
         
     
     def reload_gun(self, reload):
         if reload == 0: # Start of round 1
             self.rounds = ["L", "B", "B"]
-            self.total_rounds = 3
         elif reload == 1:
             self.rounds = ["L", "L", "L", "B", "B"]
-            self.total_rounds = 5
         elif reload == 2: # Start of round 2
             self.rounds = ["L", "B"]
-            self.total_rounds = 2
         elif reload == 3:
             self.rounds = ["L", "L", "B", "B"]
-            self.total_rounds = 4
         elif reload == 4:
             self.rounds = ["L", "L", "L", "B", "B"]
-            self.total_rounds = 5
-        elif reload == 5: # Made up (DOUBLE CHECK ON VIDEOS)
-            self.rounds = ["L", "L", "B", "B"]
-            self.total_rounds = 4
-        elif reload == 6: # Start of round 3
+        elif reload == 5:
+            self.rounds = ["L", "L", "L", "B", "B", "B"]
+        elif reload == 6:
+            self.rounds = ["L", "L", "L", "L", "L", "B", "B"]
+        elif reload == 7: # Start of round 3
             self.rounds = ["L", "B", "B"]
-            self.total_rounds = 3
-        elif reload == 7:
-            self.rounds = ["L", "L", "L", "L", "B", "B", "B", "B"]
-            self.total_rounds = 8
         elif reload == 8:
+            self.rounds = ["L", "L", "L", "L", "B", "B", "B", "B"]
+        elif reload == 9:
             self.rounds = ["L", "L", "L", "B", "B"]
-            self.total_rounds = 5
-        elif reload == 9: # Made up (DOUBLE CHECK ON VIDEOS)
-            self.rounds = ["L", "L", "L", "L", "B", "B"]
-            self.total_rounds = 6
+        elif reload == 10:
+            self.rounds = ["L", "L", "L", "L", "L", "B", "B", "B"]
         else:
             print("--Reload error--")
             quit()
+        # Maybe make this nicer, split into rounds
+            
+        self.total_rounds = len(self.rounds)
 
         if reload != 0 and reload != 2 and reload != 6:
             counter_L, counter_B = 0, 0
@@ -60,5 +55,4 @@ class Shotgun:
                     counter_L += 1
                 if i == "B":
                     counter_B += 1
-            
             print("It now contains", self.total_rounds, "rounds;", counter_L, "live,", counter_B, "blank.\n")
