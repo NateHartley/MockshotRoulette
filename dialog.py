5b4faee0aa6bee5b6572783004920327a8670654
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2024-04-26 14:05:52 +0100

    Seperate dialog into seperate file

---
+++ dialog.py
@@ -0,0 +1,58 @@
+import main
+
+def line_1():
+    print("LET'S MAKE THIS  A LITTLE MORE INTERESTING ...\n")
+    main.pause()
+    print("TWO ITEMS EACH.\n")
+    main.pause()
+    print("MORE ITEMS BEFORE EVERY LOAD.\n")
+
+
+def round_1():
+    main.pause()
+    print(u"""\u001b[33m
+    +----------------+
+    | ROUND 1 BEGINS |
+    +----------------+
+    \u001b[0m""")
+
+
+def round_1_info():
+    main.pause()
+    print("Both player's health will start at 2.\n")
+    print("3 shots are loaded into the shotgun; 1 live, 2 blank. \n")
+    main.pause()
+
+
+def round_2():
+    main.pause()
+    print(u"""\u001b[33m
+    +----------------+
+    | ROUND 2 BEGINS |
+    +----------------+
+    \u001b[0m""")
+
+
+def round_2_info():
+    main.pause()
+    print("Both player's health will start at 4.\n")
+    print("The shotgun has been emptied...\n")
+    print("2 shots are loaded into the shotgun; 1 live, 1 blank. \n")
+    main.pause()
+
+
+def round_3():
+    main.pause()
+    print(u"""\u001b[33m
+    +----------------+
+    | ROUND 3 BEGINS |
+    +----------------+
+    \u001b[0m""")
+
+
+def round_3_info():
+    main.pause()
+    print("Both player's health will start at 6.\n")
+    print("The shotgun has been emptied...\n")
+    print("3 shots are loaded into the shotgun; 1 live, 2 blank. \n")
+    main.pause()
